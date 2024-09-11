c4983783a24ed9de3dca2e3d474204d9d9f5ed84
Canwei <cwzhang23@gmail.com>
2024-09-11 15:38:22 -0700

    update cuda check

--- project/parallel_check_cuda.py
+++ project/parallel_check_cuda.py
@@ -7,7 +7,6 @@ print("MAP")
 tmap = minitorch.cuda_ops.tensor_map(jit(device=True)(minitorch.operators.id))
 out, a = minitorch.zeros((10,)), minitorch.zeros((10,))
 tmap[1, 32](*out.tuple(), 10, *a.tuple())
-print(tmap.parallel_diagnostics(level=3))
 
 # ZIP
 print("ZIP")
@@ -15,7 +14,6 @@ out, a, b = minitorch.zeros((10,)), minitorch.zeros((10,)), minitorch.zeros((10,
 tzip = minitorch.cuda_ops.tensor_zip(jit(device=True)(minitorch.operators.eq))
 print(out.tuple())
 tzip[1, 32](*out.tuple(), 10, *a.tuple(), *b.tuple())
-print(tzip.parallel_diagnostics(level=3))
 
 # REDUCE
 print("REDUCE")
@@ -23,8 +21,6 @@ out, a = minitorch.zeros((1,)), minitorch.zeros((10,))
 treduce = minitorch.cuda_ops.tensor_reduce(jit(device=True)(minitorch.operators.add))
 
 treduce[1, 1024](*out.tuple(), 1, *a.tuple(), 0, 0.0)
-print(treduce.parallel_diagnostics(level=3))
-
 
 # MM
 # print("MATRIX MULTIPLY")
