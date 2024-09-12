a2572ab4c4245f3416825a40421e09f12f736f9c
Canwei <cwzhang23@gmail.com>
2024-09-11 21:24:18 -0700

    remove test print

--- tests/test_tensor_general.py
+++ tests/test_tensor_general.py
@@ -63,9 +63,7 @@ def test_one_args(
     t1 = data.draw(tensors(backend=shared[backend]))
     name, base_fn, tensor_fn = fn
     t2 = tensor_fn(t1)
-    print(f"input: {t1}")
     for ind in t2._tensor.indices():
-        print(f"cuda: {t2[ind]}, scalar: {base_fn(t1[ind])}")
         assert_close(t2[ind], base_fn(t1[ind]))
 
 
