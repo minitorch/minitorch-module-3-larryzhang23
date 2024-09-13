51b9dc2dd85084b08702eb7b206b16dca5ca0ce5
Canwei <cwzhang23@gmail.com>
2024-09-12 19:59:22 -0700

    fix benchmark

--- project/benchmark.py
+++ project/benchmark.py
@@ -1,13 +1,8 @@
 import timeit 
-
-import random
-
 import numba
 
 import minitorch
-
-from .run_fast_tensor import datasets, FastTensorBackend 
-from .run_fast_tensor import FastTrain
+from run_fast_tensor import datasets, FastTensorBackend, FastTrain
 
 if numba.cuda.is_available():
     GPUBackend = minitorch.TensorBackend(minitorch.CudaOps)
@@ -15,10 +10,11 @@ if numba.cuda.is_available():
 if __name__ == "__main__":
     PTS = 50
     HIDDEN = 100
-    data = datasets["Xor"](PTS)
+    data = datasets["Circle"](PTS)
     run_times = 10
-    print("cpu", timeit.timeit("FastTrain(HIDDEN, backend=FastTensorBackend, max_epochs=10).train(data, 0.05)", number=run_times) / run_times)
-    print("gpu", timeit.timeit("FastTrain(HIDDEN, backend=FastTensorBackend, max_epochs=10).train(data, 0.05)", number=run_times) / run_times)
+    print("cpu", timeit.timeit("FastTrain(HIDDEN, backend=FastTensorBackend).train(data, 0.05, max_epochs=10)", number=run_times, globals=globals()) / run_times)
+    if numba.cuda.is_available():
+        print("gpu", timeit.timeit("FastTrain(HIDDEN, backend=GPUBackend).train(data, 0.05, max_epochs=10)", number=run_times, globals=globals()) / run_times)
 
 
 
