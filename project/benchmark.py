53315deba06147943149eaf33bb155355bf65a98
Canwei <cwzhang23@gmail.com>
2024-09-12 20:08:35 -0700

    fix benchmark

--- project/benchmark.py
+++ project/benchmark.py
@@ -17,9 +17,8 @@ if __name__ == "__main__":
     HIDDEN = 100
     data = datasets["Circle"](PTS)
     run_times = 10
-    print("cpu", timeit.timeit("FastTrain(HIDDEN, backend=FastTensorBackend).train(data, 0.05, max_epochs=10)", number=run_times, globals=globals()) / run_times)
+    print("cpu", timeit.timeit("FastTrain(HIDDEN, backend=FastTensorBackend).train(data, 0.05, max_epochs=100)", number=run_times, globals=globals()) / run_times / 100)
     if numba.cuda.is_available():
-        print("gpu", timeit.timeit("FastTrain(HIDDEN, backend=GPUBackend).train(data, 0.05, max_epochs=10)", number=run_times, globals=globals()) / run_times)
-
+        print("gpu", timeit.timeit("FastTrain(HIDDEN, backend=GPUBackend).train(data, 0.05, max_epochs=100)", number=run_times, globals=globals()) / run_times / 100)
 
 
